aa604abe9924dbad2cf12911e37f38abbc123a8c
Marcel JP Vissers <mjp.vissers@avans.nl>
2026-06-11 11:08:03 +0200

    Add gcc x86/x64 assembly support

--- Workshop 2/Workshop/cpp_c_asm/main.cpp
+++ Workshop 2/Workshop/cpp_c_asm/main.cpp
@@ -14,15 +14,40 @@ extern "C" int addWithAsmX86(int a, int b);
 #define FIRSTNUM    5
 #define SECONDNUM   6
 
-int main(int argc, char* argv[])
+static void logArchitecture(void)
 {
-#if defined(_M_X64)      
+    // sizeof(void*) geeft de grootte van een pointer (adres in geheugen) in bytes
+    // 32bit: 4 bytes 
+    // 64bit: 8 bytes
+    if (sizeof(void*) == 8)
+    {
+        std::cout << "Detected: 64-bit" << std::endl;
+    }
+    else if (sizeof(void*) == 4)
+    {
+        std::cout << "Detected: 32-bit" << std::endl;
+    }
+    else
+    {
+        std::cout << "sizeof(void*) = " << sizeof(void*) << std::endl;
+        std::cout << "Unknown architecture" << std::endl;
+    }
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+{
+#if defined(_M_X64)
     std::cout << "Hello CPP C ASM from x64!" << std::endl;
 #elif defined(_M_IX86)
     std::cout << "Hello CPP C ASM from x86!" << std::endl;
+#elif defined(__x86_64__)
+    std::cout << "Hello CPP C ASM from x64 (GCC)!" << std::endl;
+#elif defined(__i386__)
+    std::cout << "Hello CPP C ASM from x86 (GCC)!" << std::endl;
 #else
 #error Platform not supported
 #endif
+    logArchitecture();
     std::cout << __DATE__ << " " << __TIME__ << std::endl << std::endl; // log date and time of compilation, not runtime
 
     // demo of binary literals
@@ -40,6 +65,16 @@ int main(int argc, char* argv[])
 #elif defined(_M_IX86)
     int sum = addWithAsmX86(a, b);
     std::cout << "Using x86 assembly function to add " << a << " + " << b << " = " << sum << std::endl;
+#elif defined(__x86_64__)
+    std::cout << "Hello CPP C ASM from x64 (GCC)!" << std::endl;
+    int sum = addWithAsmX64(a, b);
+    std::cout << "Using x64 assembly function to add " << a << " + " << b << " = " << sum <<
+        std::endl;
+#elif defined(__i386__)
+    std::cout << "Hello CPP C ASM from x86 (GCC)!" << std::endl;
+    int sum = addWithAsmX86(a, b);
+    std::cout << "Using x86 assembly function to add " << a << " + " << b << " = " << sum <<
+        std::endl;
 #else
 #error Platform not supported
 #endif
@@ -47,5 +82,6 @@ int main(int argc, char* argv[])
     testcrc();
     // call C++ function to test BitSwap
     testSwap();
+
     return 0;
 }
---
+++ Workshop 2/Workshop/cpp_c_asm/readme.txt
@@ -0,0 +1,44 @@
+Linux build steps
+
+1. Open terminal
+
+2. Install gcc and g++ compilers
+sudo apt install gcc
+sudo apt install g++
+2a. Extra for x86 build
+sudo apt install g++-multilib
+sudo apt install gcc-multilib
+
+3. Install cmake and make
+sudo apt install cmake
+sudo apt install make
+
+4. Install unzip
+sudo apt install unzip
+
+5. Download the zip file from github
+wget https://github.com/AvansICT/ESE2.1-CPP/archive/refs/heads/main.zip
+
+6. Unzip the file
+unzip main.zip
+
+7. navigate to the unzipped folder
+cd ESE2.1-CPP-main/Workshop\ 2/Workshop/cpp_c_asm/
+
+8. Create a build folder and navigate to it
+mkdir build
+cd build
+
+9. Make sure g++ is used as C++ compiler
+   (note: for c files use the gcc compiler must be used)
+export CXX=/usr/bin/g++
+
+11. Run cmake
+cmake ..
+
+12. Run make
+make
+
+13. Run the program
+64bit: ./cpp_c_asm_x64
+32bit: ./cpp_c_asm_x86
